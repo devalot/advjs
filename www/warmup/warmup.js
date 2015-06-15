2e7e09ea3d33d1e6ede2c125ec823b6d23aa8a04
Peter Jones <pjones@pmade.com>
2015-06-15 10:27:40 -0600

    Initial import

---
+++ www/warmup/warmup.js
@@ -0,0 +1,18 @@
+(function(){
+
+  /*
+   * Use Case:
+   *
+   * 1. User clicks on the "Change" button.
+   *
+   * 2. If the text input is blank then nothing changes.
+   *
+   * 3. The contents of the H1 element are replaced with the contents
+   *    of the text input.
+   *
+   * 4. The text input is cleared.
+   *
+   */
+
+
+})();
