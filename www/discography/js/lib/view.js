f0c4bf8fa1ec114b66373fb0b286b483db576c3a
Peter Jones <pjones@pmade.com>
2015-06-17 17:09:33 -0600

    More JS files for the discography app

---
+++ www/discography/js/lib/view.js
@@ -0,0 +1,24 @@
+View = (function() {
+
+  // Get a template based on its ID.
+  var get = function(templateID) {
+  };
+
+  // Render a single object using the given a template.
+  // Mustache API Docs: https://github.com/janl/mustache.js
+  var render = function(template, object) {
+  };
+
+  // Render an array of objects using the given a template.
+  var renderAll = function(template, objects) {
+    return objects.map(function(e) {
+      return render(template, e);
+    }).join("");
+  };
+
+  return {
+    get:       get,
+    render:    render,
+    renderAll: renderAll,
+  };
+});
