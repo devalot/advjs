6299722b2dd48b4732e7bb8275cfd41bf8389c01
Peter Jones <pjones@pmade.com>
2015-10-20 11:16:32 -0600

    Add project commands, fix name of script

---
+++ .dir-locals.el
@@ -0,0 +1,3 @@
+((nil . ((projectile-project-compilation-cmd "npm --no-color --no-progress --no-spin install")
+         (projectile-project-test-cmd        "node bin/jasmine --no-color")
+         (projectile-project-run-cmd         "node bin/server.js"))))
