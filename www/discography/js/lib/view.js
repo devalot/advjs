64dc554298d4ae4f802f80b85d0fa402458e4141
Peter Jones <pjones@pmade.com>
2015-12-04 13:52:26 -0700

    First wave up updates from morning of day 2 of 2

--- www/discography/js/lib/view.js
+++ www/discography/js/lib/view.js
@@ -3,11 +3,20 @@ View = (function() {
   // Fetch a template based on its ID from the HTML page.  Return the
   // text content of the template without rendering it.
   var get = function(templateID) {
+    var template = document.getElementById(templateID);
+
+    if (template) {
+      return template.innerHTML;
+    } else {
+      return "INVALID TEMPLATE ID!";
+    }
   };
 
-  // Render a single object using the given a template.
-  // Mustache API Docs: https://github.com/janl/mustache.js
+  // Render a single object using the given a template and return the
+  // rendered output.  Mustache API Docs:
+  // https://github.com/janl/mustache.js
   var render = function(template, object) {
+    return Mustache.render(template, object);
   };
 
   // Given a template ID and an object, fetch the template, render it,
@@ -15,6 +24,10 @@ View = (function() {
   // template text into the `<div id="view"></div>' section of
   // index.html.
   var set = function(templateID, object) {
+    var view     = document.getElementById("view"),
+        template = get(templateID);
+
+    view.innerHTML = render(template, object);
   };
 
   return {
