5980d8e627b3b733873fb9e5c2790c715f5ea349
Peter Jones <pjones@pmade.com>
2016-03-17 13:44:31 -0600

    Changes from day 4 (before lunch) of 5

--- spec/artist.spec.js
+++ spec/artist.spec.js
@@ -15,4 +15,43 @@ describe("Artist model interface", function() {
       });
     });
   });
+
+  it("should promise an array from fetchAll", function(done) {
+    XMLHttpRequest.withResponse(function(response) {
+      // Set up fake data for testing:
+      var fakeArtists = [
+        {name: "Cut Copy",   id: 1},
+        {name: "Miike Snow", id: 2},
+      ];
+
+      // Pretend the server responded with:
+      response.responseText = JSON.stringify(fakeArtists);
+
+      // Test to see if Artist.fetchAll works:
+      Artist.fetchAll().then(function(artists) {
+        expect(Array.isArray(artists)).toBe(true);
+        expect(artists.length).toBe(2);
+        /* Add more expectations here */
+        done();
+      });
+    });
+  });
+
+  it("create then save should record the object", function(done) {
+    XMLHttpRequest.withResponse(function(response) {
+      // Need a record the server will respond with:
+      var fakeArtist = {name: "Elvis", id: 15};
+
+      // Fake the server response:
+      response.responseText = JSON.stringify(fakeArtist);
+
+      // Now test the create+save:
+      var artist = Artist.create({name: "Elvis"});
+
+      artist.save().then(function() {
+        expect(artist.id).toBe(15);
+        done();
+      });
+    });
+  });
 });
--- www/discography/js/models/artist.js
+++ www/discography/js/models/artist.js
@@ -1,8 +1,11 @@
-Artist = (function(){
+Artist = (function() {
 
   // Artist constructor.  Given an object describing an artist, copy
   // all of its local properties into `this'.
   var Artist = function(record) {
+    for (var p in record) {
+      this[p] = record[p];
+    }
   };
 
   Artist.prototype = {
@@ -10,22 +13,43 @@ Artist = (function(){
     // otherwise update an existing record.  Return a promise from the
     // Ajax library.
     save: function() {
+      if (this.id) {
+        return Ajax.patch("/api/artists/" + this.id, this);
+      } else {
+        var self = this;
+
+        return Ajax.post("/api/artists", this).
+          then(function(record) {
+            self.id = record.id;
+          });
+      }
     },
 
     // Optional: Write a `destroy' method that deletes the artist.
     // Return a promise.
     destroy: function() {
+      return Ajax.destroy("/api/artists/" + this.id);
     },
   };
 
   // Should fetch a single artist via Ajax.  Return a promise that
   // resolves to an instance of the Artist function.
   var fetchOne = function(id) {
+    return Ajax.get("/api/artists/" + id).
+      then(function(record) {
+        return new Artist(record);
+      });
   };
 
   // Should fetch all artists via Ajax.  Return a promise that
   // resolves to an array of Artist objects.
   var fetchAll = function() {
+    return Ajax.get("/api/artists").
+      then(function(records) {
+        return records.map(function(record) {
+          return new Artist(record);
+        });
+      });
   };
 
   // Public interface:
