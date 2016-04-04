9178d90db3cd143dae42ba9196b06f2ab0d29834
Peter Jones <pjones@pmade.com>
2016-04-03 19:35:23 -0600

    Add some bonus exercises to the warm-up lab

--- www/warmup/warmup.js
+++ www/warmup/warmup.js
@@ -7,11 +7,26 @@
    *
    * 2. If the text input is blank then nothing changes.
    *
-   * 3. The contents of the H1 element are replaced with the contents
+   * 3. The contents of the <h1> element are replaced with the contents
    *    of the text input.
    *
    * 4. The text input is cleared.
    *
+   * BONUS 1:
+   *
+   * Before step 3 above: Save the current text content of the <h1>
+   * element by creating an <li> element.  Set the text content of the
+   * <li> element to the text content of the <h1> element.  Find the
+   * <ul> element with the ID of "history" and insert the new <li>
+   * element as its first child.  Therefore, each time the <h1>
+   * element is changed, its current value is prepended to the <ul>
+   * element.
+   *
+   * BONUS 2:
+   *
+   * If one of the <li> elements inside the <ul> element from bonus 1
+   * is clicked, update the text content of the <h1> element with the
+   * text content of the clicked <li> element.
    */
 
 
