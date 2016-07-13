63702410c2b5f793297074ffda3851b165954772
Peter Jones <pjones@devalot.com>
2016-07-12 18:39:56 -0700

    Small improvements to the artist model test

--- spec/artist.spec.js
+++ spec/artist.spec.js
@@ -1,16 +1,24 @@
+require("../www/discography/js/lib/model.js");
 require("../www/discography/js/models/artist.js");
 require("../www/discography/js/lib/ajax.js");
 require("../www/discography/js/lib/promise.js");
 
+/******************************************************************************/
+//
+// Jasmine Docs: https://jasmine.github.io/2.4/introduction.html
+//
+/******************************************************************************/
+
 describe("Artist model interface", function() {
   it("fetchOne should provide one artist", function(done) {
     XMLHttpRequest.withResponse(function(response) {
-      var artist = {name: "The Wombats"};
-      response.responseText = JSON.stringify(artist);
+      var record = {name: "The Wombats"};
+      response.responseText = JSON.stringify(record);
 
-      Artist.fetchOne(1).then(function(record) {
-        expect(record.name).toEqual(artist.name);
-        expect(record.save).toBeDefined();
+      Artist.fetchOne(1).then(function(artist) {
+        expect(artist instanceof Artist).toBeTruthy();
+        expect(artist.name).toBe(record.name);
+        expect(artist.save).toBeDefined();
         done(); // Make sure to call `done' last.
       });
     });
