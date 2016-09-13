5892aecf80e0f8baf69e3803219efab850a883d7
Peter Jones <pjones@devalot.com>
2016-09-13 04:16:30 -0700

    Remove an answer that I some how committed :(

--- www/partial/partial.js
+++ www/partial/partial.js
@@ -2,13 +2,4 @@ Function.prototype.partial = function() {
 
   // Your code here.
 
-  var args = Array.from(arguments);
-  var context = args.shift();
-  var func = this;
-
-  return function() {
-    var newArgs = Array.from(arguments);
-    return func.apply(context, args.concat(newArgs));
-  };
-
 };
