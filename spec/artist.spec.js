9354efe5c0582bc1cacc0f0734b50565bc193992
Peter Jones <pjones@devalot.com>
2017-01-10 14:10:05 -0700

    Rename a variable so it makes a bit more sense

--- spec/artist.spec.js
+++ spec/artist.spec.js
@@ -12,18 +12,40 @@ require("../www/discography/js/lib/promise.js");
 describe("Artist model interface", function() {
   it("fetchOne should provide one artist", function(done) {
     // Some test data:
-    var artist = {name: "The Wombats"};
+    var record = {name: "The Wombats"};
 
     // Pretend the server responded with the above object:
-    ajaxSpy('get', artist);
+    ajaxSpy('get', record);
 
     // Call into the Artist model:
-    Artist.fetchOne(1).then(function(record) {
-      expect(record.name).toEqual(artist.name);
-      expect(record.save).toBeDefined();
+    Artist.fetchOne(1).then(function(artist) {
+      expect(artist.name).toEqual(record.name);
+      expect(artist.save).toBeDefined();
       done(); // Make sure to call `done' last.
     }).catch(function() {
       done.fail("shouldn't have failed");
     });
+  });
+
+  it("fetchAll should provide all artists", function(done) {
+    var records = [
+      {name: "Peter Bjorn and John"},
+      {name: "David Bowie"},
+    ];
+
+    ajaxSpy('get', records);
+
+    Artist.fetchAll().
+      then(function(artists) {
+        expect(artists.length).toBe(records.length);
+        expect(artists[0].name).toBe(records[0].name);
+        expect(artists.every(function(artist) {
+          return artist instanceof Artist;
+        })).toBeTruthy();
+        done();
+      }).
+      catch(function() {
+        done.fail("promise rejected");
+      });
   });
 });
