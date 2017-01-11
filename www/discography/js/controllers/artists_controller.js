6d8a032f73722dc5635a8ad4dbee3d26be8f0af5
Peter Jones <pjones@devalot.com>
2017-01-10 20:10:58 -0700

    Changes from day 2 of 2 (after lunch)

--- www/discography/js/controllers/artists_controller.js
+++ www/discography/js/controllers/artists_controller.js
@@ -2,15 +2,31 @@ ArtistsController = (function(){
 
   // Display all artists.
   var index = function() {
-    // 1: Fetch all artists
+    // 1. Fetch all artists.
     Artist.fetchAll().then(function(artists) {
-      // 2: Render the template
+      // 2. Render the template.
       View.set("artists-index", {artists: artists});
+
+      // 3. Make artist names links.
+      // (See template.)
+
+      // 4. Listen for clicks on artist names.
+      var ul = document.getElementById("artists-list");
+      ul.addEventListener("click", function(e) {
+        // 5. Call show() with the clicked artist ID.
+        var id = e.target.getAttribute("data-artist-id");
+        if (id) show(id);
+      });
     });
   };
 
   // Display a single artist.
   var show = function(id) {
+    // 6. Display all information about the artist
+    //    with the given ID.
+    Artist.fetchOne(id).then(function(artist) {
+      View.set("artists-show", artist);
+    });
   };
 
   // Display a form for creating a new artist.
