98296ccc16f7d229c09eb790f093283f5094bdd1
Peter Jones <pjones@devalot.com>
2017-05-22 21:13:18 -0700

    Write additional tests for the Ajax library

--- spec/ajax.spec.js
+++ spec/ajax.spec.js
@@ -6,6 +6,7 @@ describe("Ajax interface", function() {
     expect(Ajax.get).toBeDefined();
   });
 
+  /****************************************************************************/
   it("get method should resolve promise", function(done) {
     XMLHttpRequest.withResponse(function(response) {
       response.responseText = JSON.stringify([1, 2, 3]);
@@ -23,8 +24,41 @@ describe("Ajax interface", function() {
         // *after* all expectations or the tests will silently pass :(
         done();
       }).catch(function() {
-        expect("promise to be resolved").toEqual("rejected");
-        done();
+        done.fail("promise should have been resolved");
+      });
+    });
+  });
+
+  /****************************************************************************/
+  it("should reject when status is not in 200 range", function(done) {
+    XMLHttpRequest.withResponse(function(response) {
+      response.status = 199;
+
+      var promise = Ajax.get("/foo");
+      expect(promise).toBeDefined();
+      expect(promise.then).toBeDefined();
+
+      promise.then(function() {
+        done.fail("when HTTP status is 199, promise should reject");
+      }).catch(function() {
+        done(); // Tell jasmine that our test is done.
+      });
+    });
+  });
+
+  /****************************************************************************/
+  it("should reject when the `error' event fires", function(done) {
+    XMLHttpRequest.withResponse(function(response) {
+      response.status = null; // Will trigger an `error' event.
+
+      var promise = Ajax.get("/foo");
+      expect(promise).toBeDefined();
+      expect(promise.then).toBeDefined();
+
+      promise.then(function() {
+        done.fail("when `error' event fires the promise should reject");
+      }).catch(function() {
+        done(); // Tell jasmine that our test is done.
       });
     });
   });
