60dc285b4f0a8334f70c7032d71a0e35729f1318
Peter Jones <pjones@devalot.com>
2017-07-24 15:34:00 -0700

    Improve the view exercise

--- www/discography/js/lib/view.js
+++ www/discography/js/lib/view.js
@@ -1,25 +1,14 @@
 View = (function() {
 
-  // Fetch a template based on its ID from the HTML page.  Return the
-  // text content of the template without rendering it.
-  var get = function(templateID) {
-  };
-
-  // Render a single object using the given template.
+  // Given a template ID and an object, fetch the template, render it
+  // using Mustache, and insert its text into the HTML page.  Place
+  // the rendered template text into the `<div id="view"></div>'
+  // section of index.html.
+  //
   // Mustache API Docs: https://github.com/janl/mustache.js
-  var render = function(template, object) {
-  };
-
-  // Given a template ID and an object, fetch the template, render it,
-  // and insert its text into the HTML page.  Place the rendered
-  // template text into the `<div id="view"></div>' section of
-  // index.html.
   var set = function(templateID, object) {
   };
 
-  return {
-    get:    get,
-    render: render,
-    set:    set,
-  };
+  // Return the public API.
+  return {set: set};
 })();
