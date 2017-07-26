462156f6676e9ba7b0b9cdc24bb3f72b1449acaa
Peter Jones <pjones@devalot.com>
2017-07-26 13:14:02 -0700

    Remove unnecessary file: `.dir-locals.el'

--- .dir-locals.el
+++
@@ -1,3 +0,0 @@
-((nil . ((pjones-project-compile-cmd "npm --no-color --no-progress --no-spin install")
-         (pjones-project-test-cmd    "node bin/jasmine --no-color")
-         (pjones-project-run-cmd     "node bin/server.js"))))
