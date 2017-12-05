1013672b264eef5254f5bed85f7847ecfde0bd42
Peter Jones <pjones@devalot.com>
2017-12-05 12:35:23 -0700

    Build a proper WebSocket demo

--- www/apis/websockets/main.js
+++ www/apis/websockets/main.js
@@ -1,7 +1,28 @@
 $(function() {
-  var socket = io('http://localhost:3000');
-
-  socket.on('log', function (data) {
+  var log = function(data) {
     $("#log").prepend($("<li></li>").text(data));
+  };
+
+  var ws = new WebSocket("ws://localhost:3000/");
+
+  ws.onopen = function() {
+    log("connected to WebSocket server");
+  };
+
+  ws.onclose = function() {
+    log("connection to WebSocket server closed");
+  };
+
+  ws.onerror = function() {
+    log("a WebSocket error occurred");
+  };
+
+  ws.onmessage = function(e) {
+    log("incoming message: " + e.data);
+  };
+
+  $("button").click(function() {
+    log("sending PING to server");
+    ws.send("PING");
   });
 });
